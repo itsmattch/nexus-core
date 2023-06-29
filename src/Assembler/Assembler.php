e4fa5c2ce8eb9641099f823ef5c116e1a1aaeba6
itsmattch <hello@itsmattch.com>
2023-06-29 19:09:13 +0200

    Null Assemblers

--- src/Assembler/Assembler.php
+++ src/Assembler/Assembler.php
@@ -2,195 +2,36 @@
 
 namespace Itsmattch\Nexus\Assembler;
 
-use Itsmattch\Nexus\Assembler\Contract\Asynchronous;
-use Itsmattch\Nexus\Assembler\Exception\BlueprintCountException;
-use Itsmattch\Nexus\Assembler\Exception\InvalidArrayFormatException;
-use Itsmattch\Nexus\Assembler\Exception\InvalidBlueprintException;
-use Itsmattch\Nexus\Assembler\Exception\InvalidResourceException;
-use Itsmattch\Nexus\Blueprint\Blueprint;
-use Itsmattch\Nexus\Common\Exception\InvalidModelException;
-use Itsmattch\Nexus\Model\Model;
-use Itsmattch\Nexus\Action\Action;
+use Itsmattch\Nexus\Contract\Action;
+use Itsmattch\Nexus\Contract\Assembler as AssemblerContract;
+use Itsmattch\Nexus\Contract\Blueprint;
+use Itsmattch\Nexus\Contract\Model;
 
-/**
- * The Assembler class is responsible for assembling data
- * from multiple Resources into a single Model.
- */
-abstract class Assembler
+class Assembler implements AssemblerContract
 {
-    /** The class name of the model. */
-    protected string $model = Model::class;
-
-    /**
-     * A list of class names of all resources containing
-     * pieces of the model. Each resource should be a
-     * subclass of the Resource class.
-     */
-    protected string|array $resource = [];
-
-    /**
-     * The class name(s) of the blueprint(s) mapping
-     * resources to model fields. Each blueprint should be
-     * a subclass of the Blueprint class.
-     */
-    protected string|array $blueprint = [];
-
-    /** The instances of the resource classes. */
-    private array $resourcesInstance = [];
-
-    /** The instances of the blueprint classes. */
-    private array $blueprintsInstance = [];
-
-    /** The parameters to pass to the resources. */
-    private array $resourceParameters;
-
-    /** @param array $parameters An array of parameters to pass to the Resources. */
-    public function __construct(array $parameters = [])
+    //
+    public function setAction(string $name, Action $action): void
     {
-        $this->resourceParameters = $parameters;
+        // TODO: Implement setAction() method.
     }
 
-    /**
-     * Creates an instance of the Assembler class, prepares
-     * and validates it, and tries to access and read the
-     * resources.
-     *
-     * @param array $parameters An array of parameters to pass to the Resources.
-     * @return ?Assembler Returns Assembler instance if accessed successfully, null otherwise.
-     */
-    public static function load(array $parameters = []): ?Assembler
+    public function setBlueprint(Blueprint $blueprint): void
     {
-        try {
-            $instance = new static($parameters);
-
-            $instance->prepare();
-            $instance->validate();
-
-            if (!$instance->access()) {
-                return null;
-            }
-            if (!$instance->read()) {
-                return null;
-            }
-
-            return $instance;
-
-        } catch (\Exception) {
-            return null;
-        }
+        // TODO: Implement setBlueprint() method.
     }
 
-    /**
-     * Finds a specific instance of the Assembler class
-     * based on a given unique identifier.
-     *
-     * @param string $identifier A unique identifier value.
-     * @param string $parameterName An alternative identifier parameter name.
-     * @return ?Assembler Returns Assembler instance if accessed successfully, null otherwise.
-     */
-    public static function find(string $identifier, string $parameterName = 'id'): ?Assembler
+    public function setModel(string $class): void
     {
-        if (empty($identifier) || empty($parameterName)) {
-            return self::load();
-        }
-        $parameter = [$parameterName => $identifier];
-
-        return static::load($parameter);
+        // TODO: Implement setModel() method.
     }
 
-    /** Prepares the assembler for validation and processing. */
-    public function prepare(): void
+    public function assemble(): bool
     {
-        if (!is_array($this->resource)) {
-            $this->resource = [$this->resource];
-        }
-        if (!is_array($this->blueprint)) {
-            $this->blueprint = [$this->blueprint];
-        }
+        // TODO: Implement assemble() method.
     }
 
-    /**
-     * Validates the assembler's state. Throws an exception
-     * in case of a validation error.
-     *
-     * @throws BlueprintCountException
-     * @throws InvalidArrayFormatException
-     * @throws InvalidBlueprintException
-     * @throws InvalidModelException
-     * @throws InvalidResourceException
-     */
-    public function validate(): void
+    public function getModel(): Model
     {
-        // Model must represent a subclass of Model.
-        if (!is_subclass_of($this->model, Model::class)) {
-            throw new InvalidModelException($this->model);
-        }
-
-        // The number of blueprints must either be 1 or equal to number of resources.
-        if (count($this->blueprint) !== 1 && count($this->resource) !== count($this->blueprint)) {
-            throw new BlueprintCountException();
-        }
-
-        // Both arrays should be lists.
-        if (!array_is_list($this->resource) || !array_is_list($this->blueprint)) {
-            throw new InvalidArrayFormatException();
-        }
-
-        // Resources list must not contain any values other than
-        // strings representing subclasses of Resource class.
-        $filteredResources = array_filter($this->resource, function ($resourceCandidate) {
-            return is_subclass_of($resourceCandidate, Action::class);
-        });
-        if (count($filteredResources) !== count($this->resource)) {
-            throw new InvalidResourceException();
-        }
-
-        // Blueprints list must not contain any values other than
-        // strings representing subclasses of Blueprint class.
-        $filteredBlueprints = array_filter($this->blueprint, function ($blueprintCandidate) {
-            return is_subclass_of($blueprintCandidate, Blueprint::class);
-        });
-        if (count($filteredBlueprints) !== count($this->blueprint)) {
-            throw new InvalidBlueprintException();
-        }
-    }
-
-    /**
-     * Tries to access the resources by loading their
-     * instances. If a resource cannot be accessed, and the
-     * Assembler is not asynchronous,  the method will
-     * return false immediately.
-     *
-     * @return bool True if at least one resource can be accessed, false otherwise.
-     */
-    public function access(): bool
-    {
-        /** @var Action $resource */
-        foreach ($this->resource as $i => $resource) {
-            $resourceInstance = $resource::load($this->resourceParameters);
-            if ($resourceInstance === null && !($this instanceof Asynchronous)) {
-                return false;
-            }
-            $this->resourcesInstance[$i] = $resourceInstance;
-        }
-        return count(array_filter($this->resourcesInstance)) > 0;
-    }
-
-    /**
-     * Tries to read the data from the Resources using the Blueprints.
-     *
-     * @return bool True if the data can be read, false otherwise.
-     */
-    public function read(): bool
-    {
-        /** @var Action $resource */
-        foreach ($this->resourcesInstance as $i => $resource) {
-            if (count($this->blueprintsInstance) === 1) {
-                // blueprint[0] on $resource->get();
-            } else {
-                // blueprint[i] on $resource->get();
-            }
-        }
-        return true;
+        // TODO: Implement getModel() method.
     }
 }
