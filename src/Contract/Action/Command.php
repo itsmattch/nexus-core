afa3c5f94d989c440982739f13b1a2cbf2527b9a
itsmattch <hello@itsmattch.com>
2023-06-30 07:46:09 +0200

    Add action separation interfaces

---
+++ src/Contract/Action/Command.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Itsmattch\Nexus\Contract\Action;
+
+/**
+ * Command represents a performable action that
+ * changes the state of a resource.
+ */
+interface Command { }
---
+++ src/Contract/Action/Query.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Itsmattch\Nexus\Contract\Action;
+
+/**
+ * Query represents a performable action that retrieves
+ * data from a resource without altering its state.
+ */
+interface Query { }
