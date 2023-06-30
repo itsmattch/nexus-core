07b9e5c419ce9bbd3298444e55377608fc3c5c76
itsmattch <hello@itsmattch.com>
2023-06-30 07:49:26 +0200

    Move Cacheable interface

---
+++ src/Contract/Action/Cacheable.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Itsmattch\Nexus\Contract\Action;
+
+/**
+ * Cacheable indicates that the execution of an action
+ * (usually a Query) can be cached to boost performance.
+ */
+interface Cacheable { }
