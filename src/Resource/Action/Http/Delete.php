8a25d911a511e096302f45682fedb508d940aba0
itsmattch <hello@itsmattch.com>
2023-07-10 11:16:39 +0200

    Move default actions to resource directory

--- src/Resource/Action/Http/Delete.php
+++ src/Resource/Action/Http/Delete.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace Itsmattch\Nexus\Action\Http;
+namespace Itsmattch\Nexus\Resource\Action\Http;
 
 use Itsmattch\Nexus\Contract\Resource;
 use Itsmattch\Nexus\Contract\Resource\Action;
 use Itsmattch\Nexus\Engine\Enum\HttpMethod;
 
-abstract class Create implements Action
+class Delete implements Action
 {
     public function act(Resource $resource): void
     {
-        $resource->getEngine()->setMethod(HttpMethod::POST);
+        $resource->getEngine()->setMethod(HttpMethod::DELETE);
     }
 }
--- src/Resource/Action/Http/Update.php
+++ src/Resource/Action/Http/Update.php
@@ -1,15 +1,15 @@
 <?php
 
-namespace Itsmattch\Nexus\Action\Http;
+namespace Itsmattch\Nexus\Resource\Action\Http;
 
 use Itsmattch\Nexus\Contract\Resource;
 use Itsmattch\Nexus\Contract\Resource\Action;
 use Itsmattch\Nexus\Engine\Enum\HttpMethod;
 
-class Delete implements Action
+abstract class Update implements Action
 {
     public function act(Resource $resource): void
     {
-        $resource->getEngine()->setMethod(HttpMethod::DELETE);
+        $resource->getEngine()->setMethod(HttpMethod::PUT);
     }
 }
